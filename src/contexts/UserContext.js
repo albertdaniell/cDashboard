d1917078d6cb3dc4b1635bb1246a2ca32ac7efe5
albertdaniell <albertagoya@gmail.com>
2020-01-01 16:20:41 +0300

    forth  commit 2020

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -22,7 +22,7 @@ const UserContextProvider = (props) => {
     setDataPresent] = useState(false)
 
   const getUserData = async() => {
-    const userdetails = await fetch(`${constants.MY_PROXY}me/`)
+    const userdetails = await fetch(`${constants.MY_PROXY}me/`,fetchOptions)
     const userdetails2 = await userdetails.json()
 
     setUserData(userdetails2)
